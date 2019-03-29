45a492e50c9a9b100324f957427153f0534a4799
wally <18603454788@163.com>
2019-03-29 23:12:22 +0800

    复杂widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,86 +2,79 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyAppBar extends StatelessWidget {
-  // 继承widget
-  final Widget title;
-
-  MyAppBar({Key key, @required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: new BoxDecoration(
-        color: Colors.red[700],
-      ),
-      child: new Row(
-        children: <Widget>[
-          new IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: null,
-            tooltip: 'navigation menu',
-          ),
-          new Expanded(child: title),
-          new IconButton(
-              icon: Icon(Icons.search), tooltip: 'search', onPressed: null),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: new Text('title'),
-        leading:
-            new IconButton(icon: Icon(Icons.account_circle), onPressed: () {}),
-      ),
-      body: new Material(
-        child: new Column(
-          children: <Widget>[
-            // 传递widget
-            new MyAppBar(
-              title: new Text(
-                'Widget Title',
-                style: Theme.of(context).primaryTextTheme.title,
-              ),
-            ),
-            new Expanded(
-              // 包装添加事件
-              child: new GestureDetector(
-                onTap: () {
-                  print('tap this container');
-                },
-                child: new Container(
-                  decoration: new BoxDecoration(
-                    color: Colors.red[100],
-                  ),
-                  child: new Center(
-                    child: new Text('hello Expanded'),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-          onPressed: () {}, child: new Icon(Icons.add)),
-    );
-  }
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'widget概览',
-      home: new MyScaffold(),
+      title: '复杂widget',
+      home: new MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MyBody();
+  }
+}
+
+// 控制显示数字widget
+class CounterDisplay extends StatelessWidget {
+  final int count;
+
+  CounterDisplay({Key key, this.count}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text('count: $count'),
+    );
+  }
+}
+
+// 用来控制操作的widget
+class CounterIncrementor extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  CounterIncrementor({Key key, this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new FloatingActionButton(
+      child: new Icon(Icons.add),
+      onPressed: onPressed,
+    );
+  }
+}
+
+class MyBody extends StatefulWidget {
+  final Widget child;
+
+  MyBody({Key key, this.child}) : super(key: key);
+
+  _MyBodyState createState() => _MyBodyState();
+}
+
+class _MyBodyState extends State<MyBody> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('复杂widget'),
+      ),
+      body: new CounterDisplay(count: _counter),
+      floatingActionButton: new CounterIncrementor(
+        onPressed: _increment,
+      ),
     );
   }
 }
